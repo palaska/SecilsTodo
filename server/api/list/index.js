8c898b58ceace3a7b3b2d7099ed69bfb227e757c
Baris Palaska <barispalaska@gmail.com>
2016-06-16 14:57:45 +0300

    fix on crud privilages api/list

--- server/api/list/index.js
+++ server/api/list/index.js
@@ -6,12 +6,12 @@ import auth from '../../auth/auth.service';
 
 var router = express.Router();
 
-router.get('/', controller.index);
+router.get('/', auth.hasRole('admin'), controller.index);
 router.get('/mine', auth.isAuthenticated(), controller.mine);
-router.get('/:id', controller.show);
+router.get('/:id', auth.hasRole('admin'), controller.show);
 router.post('/', auth.isAuthenticated(), controller.create);
 router.put('/:id', auth.isAuthenticated(), controller.update);
-router.patch('/:id', controller.update);
-router.delete('/:id', controller.destroy);
+// router.patch('/:id', controller.update);
+router.delete('/:id', auth.isAuthenticated(), controller.destroy);
 
 module.exports = router;
